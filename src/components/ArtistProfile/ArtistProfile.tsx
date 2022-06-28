48ee8ad009ccd8f79edc8e654bebea34c70223dc
valeria-ab <offtotheraces@inbox.ru>
2022-06-28 16:34:48 +0400

    вёрстка artistPage

--- src/components/ArtistProfile/ArtistProfile.tsx
+++ src/components/ArtistProfile/ArtistProfile.tsx
@@ -5,6 +5,7 @@ import { NavLink } from 'react-router-dom';
 // @ts-ignore
 import style from './ArtistPage.scss';
 import arrowBack from '../../assets/buttons/arrowBack.png';
+import dash from '../../assets/artistProfile/dash.png';
 import learnMoreIcon from '../../assets/buttons/learnMoreIcon.png';
 import { IAppStore } from '../../store/store';
 import { ArtistResponseType } from '../../utils/api';
@@ -40,53 +41,52 @@ const ArtistProfile = (props: { artistInfo: ArtistResponseType }) => {
               alt="artist_picture"
               className={cx('profilePicture')}
             />
-          </div>
-          <div className={cx('infoBlock', {
-            infoBlock__light: theme === 'light',
-            infoBlock__dark: theme === 'dark',
-          })}
-          >
 
-            <div className={cx('artistInfo')}>
-              <span className={cx('infoBlock__padding')}>
-                {artistInfo.yearsOfLife}
-              </span>
-            </div>
-            <div
-              className={cx({
-                artistNameContainer__light: theme === 'light',
-                artistNameContainer__dark: theme === 'dark',
-              })}
+            <div className={cx('infoBlock', {
+              infoBlock__light: theme === 'light',
+              infoBlock__dark: theme === 'dark',
+            })}
             >
-              <span className={cx('artistName')}>{artistInfo.name}</span>
-            </div>
-            <div className={cx('artistDescription')}>
-              <span className={cx('infoBlock__padding')}>
-                {artistInfo.description}
-              </span>
-            </div>
-            <div className={cx('learnMoreButton')}>
-              <Button
-                value="read more"
-                width="200px"
-                type="outlined"
-                theme={theme}
-              />
-              <img
-                src={learnMoreIcon}
-                alt="learnMoreIcon"
-                width="12px"
-                height="7px"
-              />
-            </div>
 
-            <div className={cx('genresBlock')}>
-              {artistInfo.genres.map((g) => (
-                <Genre key={g._id} value={g.name} />
-              ))}
+              <div className={cx('artistInfo')}>
+                <span className={cx('infoBlock__padding')}>
+                  {artistInfo.yearsOfLife}
+                </span>
+              </div>
+              <div
+                className={cx('artistNameContainer', {
+                  artistNameContainer__light: theme === 'light',
+                  artistNameContainer__dark: theme === 'dark',
+                })}
+              >
+                <span className={cx('artistName')}>{artistInfo.name}</span>
+              </div>
+              <div className={cx('dash')}><img src={dash} alt="dash" width="30px" /></div>
+              <div className={cx('artistDescription')}>
+                {artistInfo.description}
+              </div>
+              <div className={cx('learnMoreButton')}>
+                <Button
+                  value="read more"
+                  width="200px"
+                  type="outlined"
+                  theme={theme}
+                />
+                <img
+                  src={learnMoreIcon}
+                  alt="learnMoreIcon"
+                  width="12px"
+                  height="7px"
+                />
+              </div>
+
+              <div className={cx('genresBlock')}>
+                {artistInfo.genres.map((g) => (
+                  <Genre key={g._id} value={g.name} />
+                ))}
+              </div>
             </div>
           </div>
-
         </div>
       )}
     </div>
