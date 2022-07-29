75643642cf963c064859164e512a426535939a4f
valeria-ab <offtotheraces@inbox.ru>
2022-07-29 17:38:59 +0400

    pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,156 +1,16 @@
-import { useDispatch, useSelector } from 'react-redux';
-import React, { useMemo, useState } from 'react';
-import classnames from 'classnames';
+import React, { useContext, useMemo } from 'react';
 import classNames from 'classnames/bind';
-import { AppDispatch, IAppStore } from '../../store/store';
-import { AuthorPaintingsType } from '../../utils/api';
-import style from '../ArtistProfile/ArtistPage.scss';
+import style from './style.scss';
+import { ThemeContext } from '../../contexts/ThemeContext';
 
 const cx = classNames.bind(style);
-// type PaginationPropsType = {
-//   setCurrentPage: (value: number) => void;
-//   currentPagesPortion: number;
-//   setCurrentPagesPortion: (value: number) => void;
-// }
-// export const Pagination = ({
-//   setCurrentPage,
-//   setCurrentPagesPortion,
-//   currentPagesPortion,
-// }: PaginationPropsType) => {
-//   const dispatch = useDispatch<AppDispatch>();
-//   // const [currentPage, setCurrentPage] = useState(1);
-//   const [portion, setPortion] = useState(1);
-//   const artworks = useSelector<IAppStore, Array<AuthorPaintingsType>>(
-//     (state) => state.artistPage.artistInfo.paintings,
-//   );
-//   const artworksPortionSize = useSelector<IAppStore, number>(
-//     (state) => state.artistPage.artworksPortionSize,
-//   );
-//   // const artworksCurrentPage = useSelector<IAppStore, number>(
-//   //   (state) => state.artistPage.artworksCurrentPage,
-//   // );
-//   const pagesCount = useSelector<IAppStore, number>(
-//     (state) => state.artistPage.artworksTotalPagesCount,
-//   );
-//
-//   const pages = [];
-//   for (let i = 1; i <= pagesCount; i += 1) {
-//     pages.push(i);
-//   }
-//   const portionSize = 3; // по 3 страниц показывать до точек
-//   const portionCount = Math.ceil(pagesCount / portionSize); // сколько всего pagination кнопок
-//
-//   // const [portion, setPortion] = useState(Math.ceil(artworksCurrentPage / portionSize));
-//
-//   const leftNumber = (portion - 1) * portionSize + 1;
-//   const rightNumber = portion * portionSize;
-//
-//   const onFirstPageClick = () => {
-//     setCurrentPagesPortion(1);
-//     setPortion(1);
-//   };
-//
-//   const onLastPageClick = () => {
-//     setCurrentPagesPortion(pagesCount);
-//     setPortion(portionCount);
-//   };
-//
-//   return (
-//     <div style={{ display: 'flex' }}>
-//       {
-//               portion === 1 && <button type="button" disabled>&lt;</button>
-//           }
-//       {portion > 1
-//           && (
-//           <button
-//             type="button"
-//             onClick={() => {
-//               // dispatch(setArtworksCurrentPage({
-//               //   artworksCurrentPage: (portionSize * (portion - 2)) + 1,
-//               // }));
-//
-//               setPortion(portion - 1);
-//             }}
-//           >
-//             &lt;
-//           </button>
-//           )}
-//       <div
-//         onClick={onFirstPageClick}
-//         onKeyDown={() => {
-//           console.log('keyboard listener');
-//         }}
-//         role="button"
-//         tabIndex={-1}
-//       >
-//         1
-//       </div>
-//       {/* first page click */}
-//       <div>...</div>
-//       {/* </> */}
-//       {/* )} */}
-//
-//       {pages
-//         .filter((p) => (p ? p >= leftNumber && p <= rightNumber : ''))
-//         .map((q) => (
-//           <div
-//             onKeyDown={() => {
-//               console.log('keyboard listener');
-//             }}
-//             role="button"
-//             tabIndex={-1}
-//             key={q}
-//             onClick={() => {
-//               // dispatch(setArtworksCurrentPage({
-//               //   artworksCurrentPage: (portionSize * (portion - 2)) + 1,
-//               // }));
-//               setCurrentPagesPortion(q);
-//             }}
-//           >
-//             {q}
-//           </div>
-//         ))}
-//       {currentPagesPortion !== portionCount
-//               && (
-//               <>
-//                 <div>...</div>
-//                 <div
-//                   onKeyDown={() => {
-//                     console.log('keyboard listener');
-//                   }}
-//                   role="button"
-//                   tabIndex={-1}
-//                   onClick={onLastPageClick}
-//                 >
-//                   {pagesCount}
-//                 </div>
-//                 {/* last page click */}
-//               </>
-//               )}
-//       {portionCount > portion
-//               && (
-//               <button
-//                 type="button"
-//                 disabled={portion === portionCount}
-//                 onClick={() => {
-//                   setPortion(portion + 1);
-//                   setCurrentPage(portionSize * portion + 1);
-//                 }}
-//               >
-//                 &gt;
-//               </button>
-//               )}
-//     </div>
-//   );
-// };
 
 type PaginationPropsType = {
-  onPageChange: (page: number) => void;
-  totalCount: number;
-  siblingCount: number;
-  currentPage: number;
-  pageSize: number;
-//  className: number;
+    onPageChange: (page: number) => void;
+    totalCount: number;
+    siblingCount: number;
+    currentPage: number;
+    pageSize: number;
 }
 
 const DOTS = '...';
@@ -162,7 +22,6 @@ export const Pagination = (props: PaginationPropsType) => {
     siblingCount = 1,
     currentPage,
     pageSize,
-    // className,
   } = props;
 
   const paginationRange = usePagination({
@@ -171,11 +30,7 @@ export const Pagination = (props: PaginationPropsType) => {
     siblingCount,
     pageSize,
   });
-  console.log('pagination');
-  // If there are less than 2 times in pagination range we shall not render the component
-  if (currentPage === 0 || (paginationRange && paginationRange.length < 2)) {
-    return null;
-  }
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   const onNext = () => {
     onPageChange(currentPage + 1);
@@ -187,52 +42,60 @@ export const Pagination = (props: PaginationPropsType) => {
 
   const lastPage = paginationRange && paginationRange[paginationRange.length - 1];
   return (
-    <div style={{ display: 'flex' }}>
-      {/* Left navigation arrow */}
-      <div
+    <ul
+      style={{ display: 'flex' }}
+      className={cx('pagination-container', {
+        'pagination-container_light': theme === 'light',
+        'pagination-container_dark': theme === 'dark',
+      })}
+    >
+      <li
         className={cx('pagination-item', {
           disabled: currentPage === 1,
         })}
         onClick={onPrevious}
       >
-        <div className="arrow left" />
-      </div>
+        <div className={cx('arrow left', {
+          arrow_dark: theme === 'dark',
+          arrow_light: theme === 'light',
+        })}
+        />
+      </li>
       {paginationRange && paginationRange.map((pageNumber) => {
-        // If the pageItem is a DOT, render the DOTS unicode character
         if (pageNumber === DOTS) {
           return <div className={cx('pagination-item dots')}>&#8230;</div>;
         }
 
-        // Render our Page Pills
         return (
-          <div
+          <li
             className={cx('pagination-item', {
               selected: pageNumber === currentPage,
+              selected_light: theme === 'light',
+              selected_dark: theme === 'dark',
             })}
             onClick={() => onPageChange(pageNumber)}
           >
             {pageNumber}
-          </div>
+          </li>
         );
       })}
-      {/*  Right Navigation arrow */}
-      <div
+      <li
         className={cx('pagination-item', {
           disabled: currentPage === lastPage,
         })}
         onClick={onNext}
       >
-        <div className="arrow right" />
-      </div>
-    </div>
+        <div className={cx('arrow right')} />
+      </li>
+    </ul>
   );
 };
 
 type PropsType = {
-  totalCount: number,
-  pageSize: number,
-  siblingCount: number,
-  currentPage: number,
+    totalCount: number,
+    pageSize: number,
+    siblingCount: number,
+    currentPage: number,
 }
 export const usePagination = ({
   totalCount,
@@ -241,7 +104,8 @@ export const usePagination = ({
   currentPage,
 }: PropsType) => {
   const paginationRange = useMemo(() => {
-    const totalPageCount = Math.ceil(totalCount / pageSize);
+    // const totalPageCount = Math.ceil(totalCount / pageSize);
+    const totalPageCount = 9;
 
     const range = (start: number, end: number) => {
       const length = end - start + 1;
@@ -268,14 +132,15 @@ export const usePagination = ({
     const lastPageIndex = totalPageCount;
 
     if (!shouldShowLeftDots && shouldShowRightDots) {
-      const leftItemCount = 3 + 2 * siblingCount;
+      // const leftItemCount = 3 + 2 * siblingCount;
+      const leftItemCount = 1 + 3 * siblingCount;
       const leftRange = range(1, leftItemCount);
 
       return [...leftRange, DOTS, totalPageCount];
     }
 
     if (shouldShowLeftDots && !shouldShowRightDots) {
-      const rightItemCount = 3 + 2 * siblingCount;
+      const rightItemCount = 1 + 3 * siblingCount;
       const rightRange = range(
         totalPageCount - rightItemCount + 1,
         totalPageCount,
